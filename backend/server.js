0bb2606ac14aef8169330985030cc9ca1d28cb12
NetAcad DEVASC <changeme@example.com>
2025-11-26 20:54:32 +0000

    test: update message

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cors({
 // Route test API
 app.get("/api", (req, res) => {
   res.json({
-    message: "Hello from Backend !",
+    message: "Hello CI !",
     timestamp: new Date().toISOString(),
     client: req.get('Origin') || 'unknown',
     success: true
